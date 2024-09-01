ddf6cdfa98fc9d84dc4bfe8ce181ff94c1e2df78
Kolya <kosyanovozhenin@gmail.com>
2024-09-01 20:29:09 +0300

    Добавили пути

--- gutis/authentication/views.py
+++ gutis/authentication/views.py
@@ -1,3 +1,25 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+  context = {
+    'title': 'Главная страница',
+  }
+
+  return render(request, 'authentication/home.html', context)
+
+
+def registration(request):
+  context = {
+    'title': 'Регистрация',
+  }
+
+  return render(request, 'authentication/reg.html', context)
+
+
+def login(request):
+  context = {
+    'title': 'Вход',
+  }
+
+  return render(request, 'authentication/log.html', context)
