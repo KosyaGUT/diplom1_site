16556fd7177de8bda2c04e01d5b8c7c2884ced74
Kolya <kosyanovozhenin@gmail.com>
2025-01-10 22:49:01 +0300

    Доработал вкладку "Новости"

--- gutis/news/urls.py
+++ gutis/news/urls.py
@@ -2,8 +2,15 @@ from django.urls import path
 
 from news.views import *
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 app_name = 'news'
 
 urlpatterns = [
     path('news/', news_index, name='news'),
+    path('news/<int:news_id>/', news_detail, name='news_detail'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- gutis/news/views.py
+++ gutis/news/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from news.models import News
+from django.shortcuts import render, get_object_or_404
 
 # Функция для получения только опубликованных новостей
 def get_published_news():
-  return News.objects.filter(is_published=False).order_by('-published_date')
+  return News.objects.filter(is_published=True).order_by('-published_date')
 
 # Представление для отображения списка новостей
 def news_index(request):
@@ -15,3 +16,16 @@ def news_index(request):
   }
 
   return render(request, 'home/news.html', context)
+
+def news_detail(request, news_id):
+  news_id_item = News.objects.get(news_id=news_id)
+  news_view = get_object_or_404(News, news_id=news_id)
+  news_view.views += 1  # Увеличиваем счетчик просмотров
+  news_view.save()
+
+
+  context = {
+    "news": news_id_item,
+    "news_view": news_view
+  }
+  return render(request, 'home/news_id.html', context)
